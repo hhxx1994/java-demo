a1ffdfd39c0b360318553a0356a0a68b88796bc6
huanghaoxing <huanghaoxing@youzan.com>
2018-07-22 15:34:34 +0800

    update: add groovy

---
+++ java8/src/main/java/groovy/GroovyTest.java
@@ -0,0 +1,17 @@
+package groovy;
+
+import groovy.lang.Binding;
+import groovy.lang.Script;
+import groovy.util.GroovyScriptEngine;
+
+public class GroovyTest {
+
+	public static void main(String[] args) throws Exception {
+		String path="/Users/huanghaoxing/source/java-demo/java8/src/main/resources/groovy/";
+		GroovyScriptEngine groovyScriptEngine = new GroovyScriptEngine(path);
+		Binding binding=new Binding();
+		binding.setVariable("hello","world");
+		groovyScriptEngine.run("HelloWorld.groovy",binding);
+	}
+
+}
