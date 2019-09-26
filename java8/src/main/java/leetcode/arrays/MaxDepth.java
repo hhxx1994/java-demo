25f3e4fb0333dc7b15ed9d1881443b7c12aedc36
huanghaoxing <huanghaoxing@youzan.com>
2019-09-26 10:10:56 +0800

    最大深度层次遍历

--- java8/src/main/java/leetcode/arrays/MaxDepth.java
+++ java8/src/main/java/leetcode/arrays/MaxDepth.java
@@ -1,5 +1,10 @@
 package leetcode.arrays;
 
+import javafx.util.Pair;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * @author huanghaoxing
  */
@@ -21,5 +26,28 @@ public class MaxDepth {
         int leftHeight = maxDepth(root.left);
         int rightHeight = maxDepth(root.right);
         return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+    public int maxDepth2(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<Pair<TreeNode, Integer>> queue = new LinkedList<>();
+        queue.add(new Pair<>(root, 1));
+        int max = 0;
+        while (!queue.isEmpty()) {
+            Pair<TreeNode, Integer> pair = queue.poll();
+            TreeNode treeNode = pair.getKey();
+            Integer height = pair.getValue();
+            max = Math.max(height, max);
+            if (treeNode.right != null) {
+                queue.add(new Pair<>(treeNode.right, height + 1));
+            }
+            if (treeNode.left != null) {
+                queue.add(new Pair<>(treeNode.left, height + 1));
+            }
+        }
+        return max;
+
     }
 }
