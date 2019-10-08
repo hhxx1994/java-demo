4215b54449456b615b25e018c88cae50c7071c02
huanghaoxing <huanghaoxing@youzan.com>
2019-10-08 13:27:06 +0800

    二叉树层次遍历

---
+++ java8/src/main/java/leetcode/LevelOrder.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author huanghaoxing
+ */
+public class LevelOrder {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode treeNode = queue.poll();
+                list.add(treeNode.val);
+                if (treeNode.left != null) {
+                    queue.add(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.add(treeNode.right);
+                }
+            }
+            result.add(list);
+        }
+        return result;
+    }
+}
