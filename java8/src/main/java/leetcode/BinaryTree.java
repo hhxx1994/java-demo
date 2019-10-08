d7977dd3e2f6dc510c8720692e0214462835bc89
huanghaoxing <huanghaoxing@youzan.com>
2019-10-08 13:16:49 +0800

    层次遍历

--- java8/src/main/java/leetcode/BinaryTree.java
+++ java8/src/main/java/leetcode/BinaryTree.java
@@ -178,6 +178,24 @@ public class BinaryTree {
         }
     }
 
+    public static void layerTraversal2(BinaryTreeNode root) {
+        Queue<BinaryTreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                BinaryTreeNode treeNode = queue.poll();
+                visit(treeNode);
+                if (treeNode.left != null) {
+                    queue.add(treeNode.left);
+                }
+                if (treeNode.right != null) {
+                    queue.add(treeNode.right);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
         // 构造二叉树
@@ -222,6 +240,8 @@ public class BinaryTree {
         iterativePostOrderByTwoStacks(root);
         System.out.println("二叉树层树序遍历");
         layerTraversal(root);
+        System.out.println("二叉树层树序遍历");
+        layerTraversal2(root);
 
     }
 }
