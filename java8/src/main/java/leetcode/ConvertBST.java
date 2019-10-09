d13a7643dab5412cf538a634aa1039ba60ea960a
huanghaoxing <huanghaoxing@youzan.com>
2019-10-09 13:21:32 +0800

    把二叉搜索树转换为累加树

---
+++ java8/src/main/java/leetcode/ConvertBST.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * @author huanghaoxing
+ */
+public class ConvertBST {
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        convertBST(root.right);
+        root.val += sum;
+        sum = root.val;
+        convertBST(root.left);
+        return root;
+    }
+}
