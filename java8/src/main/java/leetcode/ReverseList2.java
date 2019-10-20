46b2f1d4bbe21299c272fbfe65ed576b6b0aa4bd
huanghaoxing <huanghaoxing@youzan.com>
2019-10-20 17:28:20 +0800

    递归翻转链表

---
+++ java8/src/main/java/leetcode/ReverseList2.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+/**
+ * @author huanghaoxing
+ */
+public class ReverseList2 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        // 递归边界条件
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode last = reverseList(head.next);
+        head.next.next = head.next;
+        head.next = null;
+        return last;
+    }
+}
