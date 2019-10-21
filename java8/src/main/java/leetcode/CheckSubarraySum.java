55ed4647959be0a0b4513131d30f2b6754fc8beb
huanghaoxing <huanghaoxing@youzan.com>
2019-10-21 10:06:43 +0800

    子数据和

--- java8/src/main/java/leetcode/CheckSubarraySum.java
+++ java8/src/main/java/leetcode/CheckSubarraySum.java
@@ -23,8 +23,25 @@ public class CheckSubarraySum {
         return false;
     }
 
-
-
+    public boolean checkSubarraySum2(int[] nums, int k) {
+        int[] sum = new int[nums.length];
+        sum[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            sum[i] = sum[i - 1] + nums[i];
+        }
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end < nums.length; end++) {
+                int result = sum[end] - sum[start] + nums[start];
+                if (k != 0 && result % k == 0) {
+                    return true;
+                }
+                if (k == 0 && result == 0) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 
 
 }
