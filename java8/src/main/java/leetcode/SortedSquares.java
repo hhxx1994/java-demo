cd48e1cec19ae96304cd21f7d0d6adba8274be63
huanghaoxing <huanghaoxing@youzan.com>
2019-11-20 14:13:23 +0800

    平方数组

--- java8/src/main/java/leetcode/SortedSquares.java
+++ java8/src/main/java/leetcode/SortedSquares.java
@@ -5,7 +5,23 @@ package leetcode;
  */
 public class SortedSquares {
     public int[] sortedSquares(int[] A) {
-
-        return null;
+        int[] ret = new int[A.length];
+        int left = 0;
+        int right = A.length - 1;
+        int index = right;
+        while (index > 0) {
+            int i = A[left] * A[left];
+            int j = A[right] * A[right];
+            if (i < j) {
+                ret[index--] = j;
+                right--;
+            } else {
+                ret[index--] = i;
+                left++;
+            }
+        }
+        return ret;
     }
+
+
 }
