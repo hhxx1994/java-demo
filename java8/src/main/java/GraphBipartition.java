e0a8d6ac859c52481b64c6435480065ccee09209
huanghaoxing <huanghaoxing@youzan.com>
2020-06-17 16:34:13 +0800

    二分图校验

--- java8/src/main/java/GraphBipartition.java
+++ java8/src/main/java/GraphBipartition.java
@@ -7,48 +7,44 @@ public class GraphBipartition {
   HashSet<Integer>[] node;
   int size;
   boolean[] visited;
-  int[] parent;
+  int[] color;
+  boolean isBipartition = true;
 
-  public GraphBipartition(HashSet<Integer>[] node , int size){
+  public GraphBipartition(HashSet<Integer>[] node, int size) {
     this.node = node;
     this.size = size;
     visited = new boolean[size];
-    parent = new int[size];
+    color = new int[size];
     for (int i = 0; i < size; i++) {
-      parent[i] = -1;
+      color[i] = -1;
+    }
+    for (int i = 0; i < size; i++) {
+      if (!visited[i] ) {
+        if(!dfs(i, 0)){
+          isBipartition = false;
+          break;
+        }
+      }
     }
   }
 
-  public Iterable<Integer> adj(int i){
+  public Iterable<Integer> adj(int i) {
     return node[i];
   }
 
-  public boolean isCycle(){
-    for (int i = 0; i < size; i++) {
-      if(!visited[i]){
-        if (dfs(i,i)) {
-          return true;
-        }
-      }
-    }
-    return false;
-  }
-
-
-
-  private boolean dfs(int i , int  parent){
+  private boolean dfs(int i, int color) {
     visited[i] = true;
-    this.parent[i] = parent;
+    this.color[i] = color;
     for (Integer w : adj(i)) {
-      if(!visited[w]){
-        if (dfs(w,i)) {
-          return true;
+      if (!visited[w]) {
+        if (!dfs(w, 1 - color)) {
+          return false;
         }
-      }else if(this.parent[w] != i){
-        return true;
+      } else if (this.color[w] == color) {
+        return false;
       }
     }
-    return false;
+    return true;
   }
 
   public static void main(String[] args) {
@@ -74,9 +70,9 @@ public class GraphBipartition {
     node3.add(2);
     node[3] = node3;
 
-    GraphBipartition graph2 = new GraphBipartition(node,4 );
+    GraphBipartition graph2 = new GraphBipartition(node, 4);
 
-    System.out.println(graph2.isCycle());
+    System.out.println(graph2.isBipartition);
 
   }
 }
