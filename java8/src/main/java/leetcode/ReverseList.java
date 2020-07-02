e6f4c8c387f662c9847108b1e1c10b994088f027
huanghaoxing <huanghaoxing@youzan.com>
2020-07-02 15:48:46 +0800

    链表排序

--- java8/src/main/java/leetcode/ReverseList.java
+++ java8/src/main/java/leetcode/ReverseList.java
@@ -48,6 +48,51 @@ public class ReverseList {
       return head.next;
   }
 
+  public ListNode sortList(ListNode head) {
+      if(head == null || head.next == null)
+          return  head;
+      ListNode midNode = partition(head);
+    ListNode rightNode = midNode.next;
+    midNode.next = null;
+    ListNode left = sortList(head);
+    ListNode right = sortList(rightNode);
+
+    return merge(left,right);
+  }
+
+  public ListNode merge(ListNode l1 , ListNode l2){
+      ListNode temp = new ListNode(-1);
+      ListNode head = temp;
+      while(l1 != null && l2 != null){
+        if(l1.val < l2.val){
+          temp.next = l1;
+          l1 = l1.next;
+          temp = temp.next;
+        }else{
+          temp.next = l2;
+          l2 = l2.next;
+          temp = temp.next;
+        }
+      }
+      if(l1 != null)
+          temp.next = l1;
+      else
+        temp.next = l2;
+
+      return  head.next;
+  }
+
+
+  private ListNode partition(ListNode node){
+      ListNode fast = node.next;
+      ListNode slow = node;
+      while(fast != null && fast.next != null){
+        slow = slow.next;
+        fast= fast.next.next;
+      }
+      return slow;
+  }
+
 
     public static void main(String[] args) {
         ReverseList reverseList = new ReverseList();
---
+++ java8/src/main/java/leetcode/SortList.java
@@ -0,0 +1,7 @@
+package leetcode;
+
+/**
+ * @author huanghaoxing
+ */
+public class SortList {
+}
